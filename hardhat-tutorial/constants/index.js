9cb5c74a08ba700fce19022237e13092aa537635
Okumu Oriaro <okumu.oriaro@gmail.com>
2022-06-16 19:34:33 +0300

    completed backend

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0xd78E12F588B057eB487006036d61302f6Ac2C0d9";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
